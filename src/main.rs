b17d9586aba09ab8aadcdcd82f2d4c8853c6fb58
Marcos Adriano <marcosadriano740@gmail.com>
2025-08-19 16:12:12 -0300

    fix: input for board with more than 3 in size

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ use game::renderer::render;
 use game::{Player, TikTakToe};
 
 fn main() {
-    let mut game = TikTakToe::new(3);
+    let size = 5;
+    let number_of_boards = size * size;
+    let mut game = TikTakToe::new(size);
     let player_one = Player { value: 'X' };
     let player_two = Player { value: 'O' };
     let mut current_player = &player_one;
@@ -24,14 +26,20 @@ fn main() {
         }
         let input: usize = match input.parse() {
             Ok(value) => {
-                if !(1..=9).contains(&value) {
-                    println!("Only number between 1 and 9 are allowed.");
+                if !(1..=number_of_boards).contains(&value) {
+                    println!(
+                        "Only number between 1 and {} are allowed.",
+                        &number_of_boards
+                    );
                     continue;
                 }
                 value
             }
             Err(_) => {
-                println!("Only number between 1 and 9 are allowed.");
+                println!(
+                    "Only number between 1 and {} are allowed.",
+                    &number_of_boards
+                );
                 continue;
             }
         };
